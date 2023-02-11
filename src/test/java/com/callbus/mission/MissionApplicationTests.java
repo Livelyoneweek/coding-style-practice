5b452a4a0db97580fabaeefcce17a94f97827b15
Livelyoneweek <wlvvwhs@naver.com>
2023-02-11 14:29:48 +0900

    Initial commit

---
+++ src/test/java/com/callbus/mission/MissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.callbus.mission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
